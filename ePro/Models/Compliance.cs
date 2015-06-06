663b3177175b5fa25af490e7cbf281b4f46509ea
gnikwerdna <andrew.king.au@gmail.com>
2015-06-06 12:19:36 +1000

    0606201

---
+++ ePro/Models/Compliance.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ePro.Models
+{
+    public class Compliance
+    {
+        [Key]
+        public int ComplinanceID { get; set; }
+        public int ComplianceFormID { get; set; }
+        public int ComplianceitemsID { get; set; }
+        public string Description { get; set; }
+
+        public virtual ComplianceForm ComplianceForm { get; set; }
+        public virtual ComplianceItems ComplianceItem { get; set; }
+    }
+
+    /*
+     public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
+        public Grade? Grade { get; set; }
+
+        public virtual Course Course { get; set; }
+        public virtual Student Student { get; set; } */
+}
---
+++ ePro/Models/ProductListing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ePro.Models
+{
+    public class ProductListing
+    {
+        [Key]
+        public int ProductListingID { get; set; }
+        [Required]
+        [Display(Name="Product Name")]
+        public string ProductName { get; set; }
+        public string Source { get; set; }
+        public virtual ICollection<ComplianceForm> ComplianceForms { get; set; }
+
+
+
+    }
+}
